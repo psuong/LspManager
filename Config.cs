bb383eb90f958bac77ef0e1cacc7e22f7c1db5fd
Porrith Suong <porrith.suong@gmail.com>
2025-02-23 18:17:20 -0500

    Finishes the download, extraction, and deletion.

--- Config.cs
+++ Config.cs
@@ -6,47 +6,46 @@ using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using Tomlyn;
 
-namespace LspManager {
+namespace LspManager;
 
-    public static class ConfigHelper {
-        public static async Task<Config> Load(string path) {
-            var text = await File.ReadAllTextAsync(path);
-            return Toml.Parse(text).ToModel<Config>();
+public static class ConfigHelper {
+    public static async Task<Config> Load(string path) {
+        var text = await File.ReadAllTextAsync(path);
+        return Toml.Parse(text).ToModel<Config>();
+    }
+
+    public static Task ForEach(this Config config, [NotNull] Func<(string url, string target), Task> action) {
+        if (!Directory.Exists(config.Destination)) {
+            Directory.CreateDirectory(config.Destination);
         }
 
-        public static Task ForEach(this Config config, [NotNull] Func<(string url, string target), Task> action) {
-            if (!Directory.Exists(config.Destination)) {
-                Directory.CreateDirectory(config.Destination);
-            }
+        var tasks = new Task[config.Repositories.Count];
+        var index = 0;
+        foreach (var setting in config.Repositories) {
+            var url = $"{config.URL}{setting.Name}/releases/latest";
 
-            var tasks = new Task[config.Repositories.Count];
-            var index = 0;
-            foreach (var setting in config.Repositories) {
-                var url = $"{config.URL}{setting.Name}/releases/latest";
-
-                var task = action.Invoke((url, setting.Target));
-                tasks[index++] = task;
-            }
-            return Task.WhenAll(tasks);
+            var task = action.Invoke((url, setting.Target));
+            tasks[index++] = task;
         }
+        return Task.WhenAll(tasks);
     }
+}
 
-    public class Config {
-        [DataMember(Name = "url")]
-        public string URL { get; set; }
+public class Config {
+    [DataMember(Name = "url")]
+    public string URL { get; set; }
 
-        [DataMember(Name = "destination")]
-        public string Destination { get; set; }
+    [DataMember(Name = "destination")]
+    public string Destination { get; set; }
 
-        [DataMember(Name = "repositories")]
-        public List<Setting> Repositories { get; set; }
-    }
+    [DataMember(Name = "repositories")]
+    public List<Setting> Repositories { get; set; }
+}
 
-    public class Setting {
-        [DataMember(Name = "name")]
-        public string Name { get; set; }
+public class Setting {
+    [DataMember(Name = "name")]
+    public string Name { get; set; }
 
-        [DataMember(Name = "target")]
-        public string Target { get; set; }
-    }
+    [DataMember(Name = "target")]
+    public string Target { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -3,55 +3,56 @@ using System.IO;
 using System.IO.Compression;
 using System.Net.Http;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace LspManager {
+namespace LspManager;
 
-    public class Program {
-        public static async Task Main(string[] argv) {
-            using var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0");
+public class Program {
+    private static readonly Regex Extension = new Regex(@"\.(zip|7z)+");
+    public static async Task Main(string[] argv) {
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0");
 
-            var cwd = Environment.CurrentDirectory;
+        var config = await ConfigHelper.Load("config.toml");
+        var t = config.ForEach(async (data) => {
+            Console.WriteLine($"Fetching from url: {data.url} with target {data.target}");
+            var response = await httpClient.GetStringAsync(data.url);
+            using var jsonDoc = JsonDocument.Parse(response);
+            var root = jsonDoc.RootElement;
+            string downloadUrl = null;
+            string fileName = null;
 
-            var config = await ConfigHelper.Load("config.toml");
-            config.Destination = "D:\\porri\\Documents\\Projects\\LspManager";
-            var t = config.ForEach(async (data) => {
-                Console.WriteLine($"Fetching from url: {data.url} with target {data.target}");
-                var response = await httpClient.GetStringAsync(data.url);
-                using var jsonDoc = JsonDocument.Parse(response);
-                var root = jsonDoc.RootElement;
-                string downloadUrl = null;
-                string fileName = null;
-
-                foreach (var asset in root.GetProperty("assets").EnumerateArray()) {
-                    var name = asset.GetProperty("name").GetString();
-                    if (name.Contains(data.target)) {
-                        fileName = name;
-                        downloadUrl = asset.GetProperty("browser_download_url").GetString();
-                        break;
-                    }
+            foreach (var asset in root.GetProperty("assets").EnumerateArray()) {
+                var name = asset.GetProperty("name").GetString();
+                if (name.Contains(data.target)) {
+                    fileName = name;
+                    downloadUrl = asset.GetProperty("browser_download_url").GetString();
+                    break;
                 }
+            }
 
-                if (string.IsNullOrEmpty(downloadUrl)) {
-                    Console.WriteLine($"Failed to find: {data.target}");
-                    return;
+            Console.WriteLine(fileName);
+
+            if (string.IsNullOrEmpty(downloadUrl)) {
+                Console.WriteLine($"Failed to find: {data.target}");
+                return;
+            }
+
+            var zipPath = Path.Combine(config.Destination, fileName);
+            Console.WriteLine($"Zip Path: {zipPath}");
+
+            using (var downloadStream = await httpClient.GetStreamAsync(downloadUrl)) {
+                using (var fileStream = File.Create(zipPath)) {
+                    await downloadStream.CopyToAsync(fileStream);
                 }
+            }
+            Console.WriteLine($"Download completed for: {zipPath}");
+            var extractionPath = Path.Combine(config.Destination, Extension.Replace(fileName, string.Empty));
+            ZipFile.ExtractToDirectory(zipPath, extractionPath, true);
+            File.Delete(zipPath);
+        });
 
-                var zipPath = Path.Combine(config.Destination, fileName);
-                Console.WriteLine($"Zip Path: {zipPath}");
-
-                using (var downloadStream = await httpClient.GetStreamAsync(downloadUrl)) {
-                    using (var fileStream = File.Create(zipPath)) {
-                        await downloadStream.CopyToAsync(fileStream);
-                    }
-                }
-                Console.WriteLine($"Download completed for: {zipPath}");
-                ZipFile.ExtractToDirectory(zipPath, config.Destination, true);
-                File.Delete(zipPath);
-            });
-
-            t.Wait();
-        }
+        t.Wait();
     }
 }
